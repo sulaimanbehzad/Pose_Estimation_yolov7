d65420659a4acf881d01f4613d035e8006290f5f
Sulaiman Behzad <40688148+sulaimanbehzad@users.noreply.github.com>
2023-02-22 11:56:46 +0430

    labels are added correctly in csv file but on 3d plot has an error uk3

--- Detect_Coordinates.py
+++ Detect_Coordinates.py
@@ -20,9 +20,8 @@ param_path = 'data/out/parameters.npz'
 # you can import them to a dictionary and access the parameters
 params = dict(np.load(param_path))
 print(params.keys())
-labels2 = ['f1', 'f2', 'f3', 'f4', 'r_shoulder', 'f_shoulder', 'r_elbow', 'l_elbow'
-                                                                          'l_wrist', 'mid1', 'mid2', 'r_wrist',
-           'l_knee', 'r_knee', 'l_ankle', 'r_ankle']
+labels2 = ['l_ankle', 'l_knee', 'r_ankle', 'r_knee', 'mid1', 'f1', 'f2', 'f3', 'l_shoulder','r_shoulder', 'l_elbow',
+           'r_elbow', 'f5', 'f6', 'f7', 'f8', 'uk1', 'uk2', 'uk3']
 labels = list('ABCDEFGHIJKLMNOP')
 c = np.random.randint(1, 5, size=16)
 
@@ -83,7 +82,7 @@ def plot_points(im_left, im_right, df_left, df_right, win):
     right_xy_coord.append(camera_points_right)
 
     for cpl, cpr in zip(camera_points_left, camera_points_right):
-        print(f'cpl: {cpl} - cpr: {cpr}')
+        # print(f'cpl: {cpl} - cpr: {cpr}')
         graph.draw_circle(center_location=(cpl[0] / 2, cpl[1] / 2), radius=5, fill_color='black', line_color='red')
         cpr[0] /= 2
         cpr[0] += IM_WIDTH / 2
@@ -155,19 +154,19 @@ def draw_image_pair(im_left, im_right, graph):
         print(inst)
 
 
-def draw_image_details(im_left, im_right, df_left, df_right, win):
+def draw_image_details(im_left, im_right, df_im_left, df_right, win):
     graph = win['-GRAPH-']
     graph.erase()
     left_xy_coord = []
     right_xy_coord = []
     draw_image_pair(im_left, im_right, graph)
 
-    vals_left = df_left.loc[df_left['image'] == im_left]
+    vals_left = df_im_left.loc[df_im_left['image'] == im_left]
     vals_right = df_right.loc[df_right['image'] == im_right]
 
     output_left = pickle.loads(codecs.decode(vals_left.iloc[0]['output'].encode(), "base64"))
     output_right = pickle.loads(codecs.decode(vals_right.iloc[0]['output'].encode(), "base64"))
-    print(f'vals left: \n{output_left} \nvalse right: \n{output_right}')
+    # print(f'vals left: \n{output_left} \nvalse right: \n{output_right}')
     # output_right = pickle.loads(codecs.decode(vals_right['output'].encode(), "base64"))
     # print(f'len output: {len(output_left)} \nleft output: {output_left}')
     steps = 3  # where's the next datapoint located
@@ -179,7 +178,7 @@ def draw_image_details(im_left, im_right, df_left, df_right, win):
                 conf = output_left[steps * kid + 2]
                 if conf < 0.5:
                     continue
-        left_xy_coord.append([x_coord, y_coord])
+        left_xy_coord.append([int(x_coord), int(y_coord)])
 
     num_kpts = len(output_right) // steps
     for kid in range(num_kpts):
@@ -189,7 +188,7 @@ def draw_image_details(im_left, im_right, df_left, df_right, win):
                 conf = output_right[steps * kid + 2]
                 if conf < 0.5:
                     continue
-        right_xy_coord.append([x_coord, y_coord])
+        right_xy_coord.append([int(x_coord), int(y_coord)])
 
     # for cpl, cpr in zip(right_xy_coord, left_xy_coord):
     #     print(f'cpl: {cpl} - cpr: {cpr}')
@@ -197,13 +196,15 @@ def draw_image_details(im_left, im_right, df_left, df_right, win):
     #     cpr[0] /= 2
     #     cpr[0] += IM_WIDTH / 2
     #     graph.draw_circle(center_location=(cpr[0], cpr[1] / 2), radius=5, fill_color='black', line_color='red')
-    draw_skeleton_2D(output_left, 3, True)
-    draw_skeleton_2D(output_right, 3, False)
+    draw_skeleton_2D(output_left, left_xy_coord, 3, True)
+    draw_skeleton_2D(output_right, right_xy_coord, 3, False)
     return left_xy_coord, right_xy_coord
 
-def draw_skeleton_2D(kpts, steps, is_left):
+def draw_skeleton_2D(kpts, coords_and_label, steps, is_left):
     for sk_id, sk in enumerate(skeleton):
         r, g, b = pose_limb_color[sk_id]
+        raw_pos1 = (int(kpts[(sk[0] - 1) * steps]), int(kpts[(sk[0] - 1) * steps + 1]))
+        raw_pos2 = (int(kpts[(sk[1] - 1) * steps]), int(kpts[(sk[1] - 1) * steps + 1]))
         if is_left:
             pos1 = (int(kpts[(sk[0] - 1) * steps] // 2), int(kpts[(sk[0] - 1) * steps + 1]) // 2)
             pos2 = (int(kpts[(sk[1] - 1) * steps] // 2), int(kpts[(sk[1] - 1) * steps + 1]) // 2)
@@ -221,10 +222,22 @@ def draw_skeleton_2D(kpts, steps, is_left):
             continue
         hex_color = rgb_to_hex((r, g, b))
         hex_color = '#' + hex_color
-        print(f'RGB: {r} {g} {b} \n  hex: {hex_color}')
+        # print(f'RGB: {r} {g} {b} \n  hex: {hex_color}')
         graph.draw_line(pos1, pos2, hex_color, width=4)
-        print(f'drew line from  {pos1} to {pos2}')
-        graph.draw_text(str(sk_id), pos1, color='black')
+        # print(f'drew line from  {pos1} to {pos2}')
+        # print(f'raw pos1 {raw_pos1}')
+        if sk_id != -1:
+            graph.draw_text(str(labels2[sk_id]), pos1, color='black')
+        print(f'coords {coords_and_label} type coords {type(coords_and_label)}')
+        try:
+            ind_to_add = coords_and_label.index([raw_pos1[0], raw_pos1[1]])
+            # print(f'{ind_to_add}')
+            coords_and_label[ind_to_add].append(sk_id)
+        except:
+            pass
+        print(f'coords after {coords_and_label} type coords {type(coords_and_label)}')
+
+
 
 def pixel_to_world(camera_intrinsics, r, t, img_points):
     K_inv = camera_intrinsics.I
@@ -264,40 +277,48 @@ def DLT(P1, P2, point1, point2):
     from scipy import linalg
     U, s, Vh = linalg.svd(B, full_matrices=False)
 
-    print('Triangulated point: ')
-    print(Vh[3, 0:3] / Vh[3, 3])
+    # print('Triangulated point: ')
+    # print(Vh[3, 0:3] / Vh[3, 3])
     return Vh[3, 0:3] / Vh[3, 3]
 
 
 def plot_keypoints_3d(lkpts, rkpts, P1, P2):
-    print('plot 3d')
+    # print('plot 3d')
+    p3ds = []
     for itr1, itr2 in zip(lkpts, rkpts):
-        p3ds = []
-        for lcp, rcp in zip(itr1, itr2):
-            _p3d = DLT(P1, P2, lcp, rcp)
-            p3ds.append(_p3d)
-        p3ds = np.array(p3ds)
+        itr1, itr2 = combine_labels(itr1, itr2)
+        _p3d = DLT(P1, P2, itr1[0:2], itr2[0:2])
+        if len(itr1) < 3:
+            _p3d = np.append(_p3d, -1)
+        else:
+            _p3d = np.append(_p3d, itr2[2])
+        p3ds = np.append(p3ds, _p3d)
+        # p3ds = np.array(p3ds)
+    # print(f'len p3ds {len(p3ds)}')
+    p3ds = np.reshape(p3ds, (16, 4))
+    # for p in p3ds:
+    #     print(f'3d {p}')
 
-        for p in p3ds:
-            print(p)
+    from mpl_toolkits.mplot3d import Axes3D
 
-        from mpl_toolkits.mplot3d import Axes3D
+    # min_thresh = np.min(p3ds)
+    # max_thresh = np.max(p3ds)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.set_xlim3d(np.min(p3ds[0]), np.max(p3ds[0]))
+    ax.set_ylim3d(np.min(p3ds[1]), np.max(p3ds[1]))
+    ax.set_zlim3d(np.min(p3ds[2]), np.max(p3ds[2]))
+    prev_p = []
+    for p, l in zip(p3ds, lkpts):
+        ax.scatter(xs=p[0], ys=p[1], zs=p[2], c='red', s=3)
+        ax.text(p[0], p[1], p[2], (str(labels2[int(p[3])])), size=5, zorder=1, color='k')
 
-        # min_thresh = np.min(p3ds)
-        # max_thresh = np.max(p3ds)
-        fig = plt.figure()
-        ax = fig.add_subplot(projection='3d')
-        ax.set_xlim3d(np.min(p3ds[0]), np.max(p3ds[0]))
-        ax.set_ylim3d(np.min(p3ds[1]), np.max(p3ds[2]))
-        ax.set_zlim3d(np.min(p3ds[2]), np.max(p3ds[2]))
-        prev_p = []
-        for p in p3ds:
-            ax.scatter(xs=p[0], ys=p[1], zs=p[2], c='red', s=3)
-            # if len(prev_p) > 0:
-            #     ax.plot([prev_p[0], p[0]], [prev_p[1], p[1]], [prev_p[2], p[2]], color='black')
-            # prev_p = p
-        # show origin point
-        ax.scatter(0, 0, 0, c='black', s=5)
+        # if len(prev_p) > 0:
+        #     ax.plot([prev_p[0], p[0]], [prev_p[1], p[1]], [prev_p[2], p[2]], color='black')
+        # prev_p = p
+    # show origin point
+    ax.scatter(0, 0, 0, c='black', s=5)
+
 
 
 # left_camera_points = plot_points(left_kpts)
@@ -351,21 +372,42 @@ def plot_keypoints_3d(lkpts, rkpts, P1, P2):
 #     ax.plot(xs=[p3ds[_c[0], 0], p3ds[_c[1], 0]], ys=[p3ds[_c[0], 1], p3ds[_c[1], 1]],
 #             zs=[p3ds[_c[0], 2], p3ds[_c[1], 2]], c='red')
 def XYZ_coords_to_csv(left_points, right_points, P1, P2, output_path):
-    df = pd.DataFrame(columns=['image_index', 'kpt_x', 'kpt_y', 'kpt_z'])
+    df = pd.DataFrame(columns=['image_index', 'kpt_x', 'kpt_y', 'kpt_z', 'label'])
     image_num = 1
     for itr_im1, itr_im2 in zip(left_points, right_points):
         for itr1, itr2 in zip(itr_im1, itr_im2):
+            itr1, itr2 = combine_labels(itr1, itr2)
             p3ds = []
-            for lcp, rcp in zip(itr1, itr2):
-                _p3d = DLT(P1, P2, lcp, rcp)
-                p3ds.append(_p3d)
+            # print(f'=========== {itr1[0:2]}')
+            _p3d = DLT(P1, P2, itr1[0:2], itr2[0:2])
+            if len(itr1) < 3:
+                _p3d = np.append(_p3d, -1)
+            else:
+                _p3d = np.append(_p3d, itr1[2])
+            p3ds.append(_p3d)
             p3ds = np.array(p3ds)
             for p in p3ds:
-                df2 = pd.DataFrame.from_records([{'image_index': image_num, 'kpt_x': int(p[0]), 'kpt_y': int(p[1]),
-                                                  'kpt_z': int(p[2])}])
+                if p[3] != -1:
+                    df2 = pd.DataFrame.from_records([{'image_index': image_num, 'kpt_x': int(p[0]), 'kpt_y': int(p[1]),
+                                                  'kpt_z': int(p[2]), 'label': labels2[int(float(p[3]))]}])
+                else:
+                    df2 = pd.DataFrame.from_records([{'image_index': image_num, 'kpt_x': int(p[0]), 'kpt_y': int(p[1]),
+                                                      'kpt_z': int(p[2]), 'label': int(p[3])}])
                 df = pd.concat([df, df2])
         image_num += 1
     df.to_csv(output_path, mode='w+', index=False)
+
+
+def combine_labels(left_prop, right_prop):
+    print(f'left point: {left_prop} right point: {right_prop}')
+    if len(left_prop) > len(right_prop):
+        right_prop.append(left_prop[2])
+    elif len(left_prop) < len(right_prop):
+        left_prop.append(right_prop[2])
+
+    print(f'AFTER\nleft point: {left_prop} right point: {right_prop}')
+
+    return left_prop, right_prop
 
 if __name__ == "__main__":
 
@@ -384,8 +426,8 @@ if __name__ == "__main__":
     RT2 = np.concatenate([R, T], axis=-1)
     projection_matrix_2 = mtx2 @ RT2  # projection matrix for C2
 
-    for l in labels:
-        print(l)
+    # for l in labels:
+    #     print(l)
     # Read csv files
     df_left = pd.read_csv(left_kpts)
     df_right = pd.read_csv(right_kpts)
@@ -470,10 +512,11 @@ if __name__ == "__main__":
                                          window)
                 left_camera_points.append(lfp)
                 right_camera_points.append(rfp)
+                # combine_labels(left_camera_points, right_camera_points)
                 itr_left += 1
                 itr_right += 1
-    print(f'size lfps: {len(left_camera_points)}')
-    print(left_camera_points)
+    # print(f'size lfps: {len(left_camera_points)}')
+    # print(left_camera_points)
     XYZ_coords_to_csv(left_camera_points, right_camera_points, projection_matrix_1, projection_matrix_2,
                       'data/out/XYZ_Coords.csv')
     plot_keypoints_3d(left_camera_points[1], right_camera_points[1], projection_matrix_1, projection_matrix_2)
